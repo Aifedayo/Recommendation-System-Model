f5a9c64fbada10d6ca61812b591b6b1cfd74f7a7
Aifedayo <akeemifedayolag@gmail.com>
2024-11-14 17:33:50 +0100

    return the engrg'ed data

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,6 +14,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 def clean_release_date(date):
     return str(date).split('-')[0]
 
+def drop_columns(df, cols):
+    return df.drop(cols, axis=1)
+
 @dataclass
 class DataIngestionConfig:
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
@@ -33,9 +36,20 @@ class DataIngestion:
                              movies_df['vote_average'], movies_df['popularity']], axis=1)
 
             movies_subset_df['release_date'] = movies_subset_df['release_date'].apply(clean_release_date)
-            movies_subset_df = movies_subset_df.fillna('')
+            movies_subset_df = self.feature_engineering(movies_subset_df)
+            
+            os.makedirs(os.path.dirname(self.ingestion.raw_data_path), exist_ok=True)
+            movies_subset_df.to_csv(self.ingestion.raw_data_path, index=False, header=True)
+            
+            return movies_subset_df
+
         except Exception as e:
             return str(e)
+
+    def feature_engineering(self, movies_subset_df):
+        movies_subset_df = movies_subset_df.fillna('')
+        cols_to_drop = ['title', 'release_date', 'cast']
+        movies_subset_df = drop_columns(movies_subset_df, cols_to_drop)
         
 
 
