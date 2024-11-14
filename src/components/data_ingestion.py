2598687cf014baa15d7f69605c4481c2cc71b356
Aifedayo <akeemifedayolag@gmail.com>
2024-11-14 17:11:49 +0100

    Add a clean_release_date function

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,9 +11,33 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+def clean_release_date(date):
+    return str(date).split('-')[0]
 
 @dataclass
 class DataIngestionConfig:
-    raw_data_path: str = os.path.join('artifacts', 'movies.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            movies_df = pd.read_csv('src/data/movies.csv')
+
+            # DataFrame cleaning
+            # Create a subset  of the df since we won't be needing everything
+            movies_subset_df = pd.concat([movies_df['genres'], movies_df['title'], movies_df['cast'], movies_df['release_date'], movies_df['director'],
+                             movies_df['vote_average'], movies_df['popularity']], axis=1)
+
+            movies_subset_df['release_date'] = movies_subset_df['release_date'].apply(clean_release_date)
+            movies_subset_df = movies_subset_df.fillna('')
+        except Exception as e:
+            return str(e)
+        
+
+
 def recommend_movie(movie_name):
-    
+    pass
