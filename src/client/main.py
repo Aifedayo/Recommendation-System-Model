76a4d4008d96230e60ad7e63318b250444bf19ae
Aifedayo <akeemifedayolag@gmail.com>
2024-11-18 21:39:42 +0100

    client side implementation

--- src/client/main.py
+++ src/client/main.py
@@ -1,3 +1,18 @@
-import pickle
 import joblib
 import streamlit as st
+
+from src.components import (
+    data_ingestion, data_transformation, movie_recommendation_by_title)
+
+def movie_recommendation_by_title(input_data):
+    obj = data_ingestion.DataIngestion()
+    clean_data = obj.initiate_data_ingestion()
+
+    dt = data_transformation.DataTransformation()
+    movie_data, cleaned_data = dt.initiate_data_transformation(clean_data)
+
+    movie_recom = movie_recommendation_by_title.MovieRecommendation()
+    recommendations = movie_recom.initiate_movie_recommendation('iron man', movie_data)
+    movie_recom = movie_recommendation_by_title.MovieRecommendation()
+    recommendations = movie_recom.initiate_movie_recommendation('iron man', movie_data)
+    movie_recommendation_by_title(input_data='Iron man')
